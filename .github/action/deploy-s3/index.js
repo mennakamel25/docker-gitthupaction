ff0ac374c9302443d0fd8f517fd916f08c6eb8ee
menna kamel <menna@example.com>
2026-05-07 15:55:11 +0300

    now

--- .github/action/deploy-s3/index.js
+++ .github/action/deploy-s3/index.js
@@ -1,6 +1,6 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";  
-import * as exec from "@actions/exec";  
+
 
 function run(){
     core.info("Hello world from my first github action by js");
