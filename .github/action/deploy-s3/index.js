7c158c0a80f78b1744df41b153d6b0c039a3585d
menna kamel <menna@example.com>
2026-05-07 22:05:30 +0300

    now

--- .github/action/deploy-s3/index.js
+++ .github/action/deploy-s3/index.js
@@ -1,16 +1,24 @@
 import * as core from "@actions/core";
-import * as github from "@actions/github";  
+import * as github from "@actions/github";
 import * as exec from "@actions/exec";
 
-function run(){
-    // Get the inputs from the action configuration
-    const bucketName = core.getInput("bucket-name" ,{required: true});
-    const region = core.getInput("region" ,{required: true});
-    const directory = core.getInput("directory" ,{required: true});
- 
-    //upload the files to s3 bucket
+async function run() {
+
+    // Get inputs
+    const bucketName = core.getInput("bucket-name", { required: true });
+
+    const region = core.getInput("region", { required: true });
+
+    const directory = core.getInput("directory", { required: true });
+
+    // Build S3 path
     const s3Uri = `s3://${bucketName}`;
+
+    // Build AWS CLI command
     const command = `aws s3 sync ${directory} ${s3Uri} --region ${region}`;
-  
+
+    // Execute command
+    await exec.exec(command);
 }
+
 run();
